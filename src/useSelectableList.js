861f8db773d41341e328892f55564527da58a82e
Yago Pereira Azevedo <56167866+YPAzevedo@users.noreply.github.com>
2020-09-28 08:20:38 -0300

    better conditionals

--- src/useSelectableList.js
+++ src/useSelectableList.js
@@ -2,13 +2,11 @@ import { useState, useCallback } from "react";
 
 const useStelectableList = (inititalList, config = {}) => {
   const [list, setList] = useState(
-    inititalList.map((item) => ({ ...item, selected: false }))
+    inititalList.map((item) => ({ ...item, selected: !!item.selected }))
   );
 
   const forceSetList = useCallback((newList) => {
-    setList(
-      newList.map((item) => ({ ...item, selected: item.selected || false }))
-    );
+    setList(newList.map((item) => ({ ...item, selected: !!item.selected })));
   }, []);
 
   const isAllSelected = list.every((item) => item.selected);
@@ -30,7 +28,7 @@ const useStelectableList = (inititalList, config = {}) => {
   const toggleAll = useCallback(() => {
     setList((listState) =>
       listState.map((item, i) => {
-        return { ...item, selected: isAllSelected ? false : true };
+        return { ...item, selected: !isAllSelected };
       })
     );
   }, [isAllSelected]);
